e105a87d77fb45c60bfe261c29624719b285d499
Anuradha1204 <165755995+Anuradha1204@users.noreply.github.com>
2024-04-02 13:15:25 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,6 +26,9 @@ if ing_list:
 
     for fruit_chossen in ing_list:
         ing_string += fruit_chossen + ', '
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+
     #st.write(ing_string)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
@@ -39,9 +42,6 @@ if ing_list:
 
         st.success('Your Smoothis is Ordered!', icon = "✅")
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-#st.text(fruityvice_response.json())
-fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
 
    
